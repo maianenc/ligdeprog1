f99fb18c551c8893b1b1a5d0014caf98525aca54
Maiane <55857165+maiann00@users.noreply.github.com>
2020-03-12 22:07:07 -0300

    Add files via upload

---
+++ time-conversion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void){
+    int n;
+    int emDias;
+    printf("digite sua idade\n\n\n");
+    scanf("%d", &n);
+    emDias = n*365;
+    printf("\nEm dias: %d", emDias);
+return 0;
+}
